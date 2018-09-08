b3ac5983f38dc17dc14f7ff7408326708aa683b5
Dimitrios Tsouvalas <Dimitrios.Tsouvalas@mistrasgroup.gr>
2018-09-09 00:23:53 +0300

    Added database for application data

---
+++ DBs/ApplicationData/dbo/tables/Movies.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[Movies]
+(
+	[Id] INT NOT NULL,
+	[Title] NVARCHAR(100) NOT NULL,
+	[Description] NVARCHAR(1000) NULL,
+	[DateAdded] DATETIME2(3) NOT NULL,
+	[UserId] NVARCHAR(128) NOT NULL,
+	CONSTRAINT [PK_Movies] PRIMARY KEY ([Id])
+)
+
+GO
+
+ALTER TABLE [dbo].[Movies] 
+	ADD CONSTRAINT [DF_Movies_DateAdded] 
+	DEFAULT SYSUTCDATETIME() 
+	FOR DateAdded
+GO  
+
+CREATE INDEX [IX_Movies_Users] ON [dbo].[Movies] ([UserId])
---
+++ DBs/ApplicationData/dbo/tables/MoviesUsersDisposition.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[MoviesUsersDisposition]
+(
+	[Id] INT NOT NULL,
+	[MovieId] INT NOT NULL,
+	[UserId] NVARCHAR(128) NOT NULL,
+	[Like] BIT NOT NULL,
+	[DateTimeSet] DATETIME2(3) NOT NULL,
+	CONSTRAINT [PK_MoviesUsersDisposition]	PRIMARY KEY ([Id]),
+	CONSTRAINT [FK_MoviesUsersDisposition_Movies] FOREIGN KEY (MovieId) REFERENCES dbo.Movies ([Id])
+	ON UPDATE CASCADE
+	ON DELETE CASCADE,
+	CONSTRAINT [AK_MoviesUsersDisposition_MoviePerUser] UNIQUE ([MovieId], [UserId]), 
+)
+
+GO
+
+ALTER TABLE dbo.MoviesUsersDisposition
+	ADD CONSTRAINT [DF_MoviesUsersDisposition_DateTimeSet] 
+	DEFAULT SYSUTCDATETIME() 
+	FOR DateTimeSet
+
+GO
