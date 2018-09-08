426efc4cc5600dc1f3ebca8a87bceb2758ea80f3
Dimitrios Tsouvalas <Dimitrios.Tsouvalas@mistrasgroup.gr>
2018-09-08 23:10:20 +0300

    Add project files.

---
+++ MovieRama/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieRama.Startup))]
+namespace MovieRama
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
