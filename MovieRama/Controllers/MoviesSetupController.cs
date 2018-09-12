221e8c3c4be3d8b22ed08102fdbcf3a20e5af99f
Dim Tsouval <dtsouval@gmail.com>
2018-09-12 23:53:31 +0300

    Added ability for user to change his/her diposition

--- MovieRama/Controllers/MoviesSetupController.cs
+++ MovieRama/Controllers/MoviesSetupController.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNet.Identity;
+using MovieRama.ModelsView.MoviesSetup;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
-using MovieRama.ModelsView.MoviesSetup;
+using NHome = MovieRama.ModelsView.Home;
 
 namespace MovieRama.Controllers
 {
@@ -42,7 +42,8 @@ namespace MovieRama.Controllers
 
             using (var dbc = new AppDAL.ApplicationDataDbContext())
             {
-                dbc.Movies.Add(new AppDAL.Movies() {
+                dbc.Movies.Add(new AppDAL.Movies()
+                {
                     Title = movieTitle,
                     Description = collection.GetValue(nameof(AppDAL.Movies.Description)).AttemptedValue,
                     UserId = User.Identity.GetUserId()
@@ -59,6 +60,86 @@ namespace MovieRama.Controllers
             }
 
             return View();
+        }
+
+        public async Task<ActionResult> UserDisposition()
+        {
+            // Get items from query string
+            if (!int.TryParse(Convert.ToString(Request.QueryString["movieId"]), out int iRequestMovieId))
+            {
+                throw new HttpException(400, "Invalid Movie");
+            }
+            if (!int.TryParse(Convert.ToString(Request.QueryString["value"]), out int iRequestDisposition))
+            {
+                throw new HttpException(400, "Invalid Disposition");
+            }
+            //
+            var currentUserId = User.Identity.GetUserId();
+            //
+            using (var dbc = new AppDAL.ApplicationDataDbContext())
+            {
+                // get user who added requested movie
+                var IsFromCurrentUser =
+                    dbc.Movies
+                    .Where(x => x.Id == iRequestMovieId && x.UserId.Equals(currentUserId))
+                    .Any();
+
+                if (IsFromCurrentUser)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+                // Get current user disposition
+                try
+                {
+                    var currentUserDispositionForMovie =
+                        dbc.MoviesUsersDisposition
+                        .Where(x => x.MovieId == iRequestMovieId && x.UserId.Equals(currentUserId))
+                        .First();
+
+                    //
+                    currentUserDispositionForMovie.DateTimeSet = DateTime.UtcNow;
+                    currentUserDispositionForMovie.MovieId = iRequestMovieId;
+                    currentUserDispositionForMovie.UserId = currentUserId;
+                    currentUserDispositionForMovie.Like = iRequestDisposition == (int)NHome.Definitions.DispositionResult.Like;
+                }
+                catch (ArgumentNullException)
+                {
+                    dbc.MoviesUsersDisposition.Add(new AppDAL.MoviesUsersDisposition()
+                    {
+                        DateTimeSet = DateTime.UtcNow,
+                        MovieId = iRequestMovieId,
+                        UserId = currentUserId,
+                        Like = iRequestDisposition == (int)NHome.Definitions.DispositionResult.Like,
+                    });
+                }
+                catch (InvalidOperationException ex)
+                {
+                    dbc.MoviesUsersDisposition.Add(new AppDAL.MoviesUsersDisposition()
+                    {
+                        DateTimeSet = DateTime.UtcNow,
+                        MovieId = iRequestMovieId,
+                        UserId = currentUserId,
+                        Like = iRequestDisposition == (int)NHome.Definitions.DispositionResult.Like,
+                    });
+                }
+                catch (Exception ex)
+                {
+                    throw new HttpException(500, $"Something went wrong. {ex.Message}");
+                }
+
+                //
+                try
+                {
+                    await dbc.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+                    throw new HttpException(500, $"Could not save data. {ex.Message}");
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
